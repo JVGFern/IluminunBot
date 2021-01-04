37fcd5e491507f9f5fc3fd7e870022f6af1ba8ff
JVGFern <jvgf.br@gmail.com>
2021-01-04 13:16:41 -0300

    finished

---
+++ commands/admin/unban.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+name: "unban",
+category: "admin",
+permissions: ['BAN_MEMBERS'],
+requiredRoles: [],
+
+
+run: async(bot, message, args ) => {
+
+    let toBan = await bot.users.fetch(args[0])
+
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need permissions!") 
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot need permissions!") 
+
+    let reason = args.slice(1).join(' ')
+    if (!reason) reason = 'No reason provided'
+
+    message.guild.members.unban(toBan, reason)
+    
+
+
+const success = new MessageEmbed()
+  .setTitle(`Successfully Unbanned: ${toBan.username}`)
+  .setDescription(`Reason: ${reason}`)
+  .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
+  .setColor('#77dd77')
+  .setFooter('Permissions Granted')
+  .setTimestamp()
+
+message.channel.send(success)
+
+}
+ 
+}
--- configs/command.js
+++ configs/command.js
@@ -1,27 +1,13 @@
+const Discord = require("discord.js");
 const { readdirSync } = require("fs");
 
-const ascii = require("ascii-table");
-
-let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
-
-module.exports = (client) => {
+module.exports = (bot) => {
     readdirSync("./commands/").forEach(dir => {
         const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
     
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
-    
-            if (pull.name) {
-                client.commands.set(pull.name, pull);
-                table.addRow(file, '✅');
-            } else {
-                table.addRow(file, `❌  ->  Algo está errado`);
-                continue;
-            }
-    
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-        }
-    });
-
-}
+            let cmd = require(`../commands/${dir}/${file}`);
+            bot.commands.set(cmd.name, cmd); 
+     }
+});
+};
