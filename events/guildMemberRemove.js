2f1e60eed49cc1ca47df508dcb5521560471f461
João <jvgf.br@gmail.com>
2021-01-23 20:26:28 -0300

    finished

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,25 +1,28 @@
-const { MessageAttachment, MessageEmbed } = require('discord.js')
-const Canvas = require('canvas')
+const { MessageEmbed, MessageAttachment } = require('discord.js')
 const { getChannelID } = require('../commands/admin/set-welcome')
 
 
-module.exports = async (bot,member) => {
+
+module.exports = async (bot, member) => {
 
     const { guild } = member
     const ChannelID= getChannelID(guild.id)
     const channel = guild.channels.cache.get(ChannelID)
     
-    const embedAdd = new  MessageEmbed ()
-    .setTitle('Welcome, my brother')
+
+    const embedRemove = new  MessageEmbed ()
+    .setTitle('Member Left')
       .setAuthor(`${member.guild.name}`, member.guild.iconURL({ dynamic: true }))
       .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
       .setDescription(`${member} (**${member.user.tag}**)`)
       .setTimestamp()
       .setColor('RANDOM');
   
-    channel.send(embedAdd);
+    channel.send(embedRemove);
    
     
+    
+
   
     }
   
