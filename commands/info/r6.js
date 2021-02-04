8e3fe554e004e1bc409cf3f467caea7137f96343
JVGFern <jvgf.br@gmail.com>
2021-02-04 18:54:44 -0300

    r6

--- commands/info/r6.js
+++ commands/info/r6.js
@@ -1,5 +1,6 @@
 const R6StatsAPI = require('r6statsapi').default
 const { MessageEmbed } = require('discord.js')
+const { Menu } = require('discord.js-menu');
 const API = new R6StatsAPI('6ac7838a-2600-4b97-8c33-c929431cedfb')
 
 module.exports = {
@@ -12,25 +13,70 @@ module.exports = {
         let plataform =  args.slice(1);
         
         let userStats = await API.getGenericStats(user, plataform, 'all');
+        let userStatsOP = await API.getOperatorStats(user, plataform, 'all');
+        let helpMenu = new Menu(message.channel, message.author.id, [
+          {
+            name: 'Stats R6',
+            content: new MessageEmbed()
+            .setTitle('Your Stats  R6')
+            .setDescription('**Goblal Stats** 1️⃣ ' + "\n" +  '**Operator Stats:** 2️⃣')
+            .setColor('RANDOM')
+            
+            
+            ,
+            reactions:{
+            '1️⃣': "next",
+            //'2️⃣': "last"
+            }
+            },
+            {
+              name: 'Stats Goblal R6',
+              content: new MessageEmbed()
+              .setColor("RANDOM")
+              .setTitle(`Your  stats goblal in r6  ${message.author.username}`)
+              .setDescription(`**Username R6:** ${userStats.username} ` + "\n" +
+              `**Level R6:** ${userStats.progression.level} `)
+              .setThumbnail(userStats.avatar_url_256)
+              .addFields(
+                { name: "kills", value: "```" + userStats.stats.general.kills + "```", inline: true },
+                { name: "headshots", value: "```" + userStats.stats.general.headshots + "```", inline: true },
+                { name: "wins", value: "```" + userStats.stats.general.wins + "```", inline: true },
+                { name: "losses", value: "```" + userStats.stats.general.losses + "```", inline: true },
+                { name: "games played", value: "```" + userStats.stats.general.games_played + "```", inline: true },
+                { name: "kd", value: "```" + userStats.stats.general.kd + "```", inline: true },
+              )
+              
+              
+              ,
+              reactions:{
+              '⏪': "first",
+              //'2️⃣': "last"
+              
+              }
+              },
+              /*{
+                name: 'Stats Operators R6',
+                content: new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle(`your Operators stats in r6  ${message.author.username}`)
+                .setDescription(`**Username R6:** ${userStats.username} ` + "\n" +
+                `**Level R6:** ${userStats.progression.level} `)
+                .setThumbnail(userStats.avatar_url_256)
+                .addFields(
+                  { name: "kills", value: "```" + userStatsOP.operators({name: 'Castel'}) + "```", inline: true },
+                 
+                )
+                
+                
+                ,
+                reactions:{
+                '⏪': "first",
+                '1️⃣': "previous"
+                
+                }
+                },*/
         
-        const r6Embed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle(`Seu stats goblal no r6  ${message.author.username}`)
-        .addFields(
-          { name: "kills", value:  "```" + userStats.stats.general.kills + "```", inline: true },
-          { name: "headshots", value: "```" + userStats.stats.general.headshots + "```", inline: true}, 
-          { name: "wins", value:  "```" + userStats.stats.general.wins + "```", inline: true }, 
-          { name: "losses", value:  "```" + userStats.stats.general.losses + "```", inline: true }, 
-          { name: "games played", value:  "```" + userStats.stats.general.games_played + "```", inline: true }, 
-          { name: "kd", value:  "```" + userStats.stats.general.kd + "```", inline: true }, 
-        )
-       
-  
-        message.channel.send(r6Embed);
-        
-
-    
-
-
-    }
+      ], 300000)
+      helpMenu.start()
+      }
 }
--- events/message.js
+++ events/message.js
@@ -4,16 +4,23 @@ const Discord = require("discord.js");
 
 
 const config = require("../config.json");
-const prefix = require('../models/prefix');
+const PrefixSchema = require('../models/prefix');
 
 module.exports = async (bot, message, guild) => {
-  
+    let prefix;
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    const data = await prefix.findOne({
+    const data = await PrefixSchema.findOne({
       GuildID: message.guild.id
   });
+  if(data){
+    prefix = data.Prefix;
+  }
+  else if (!data){
+    prefix = "#";
+  }
+
   function convertMs(mills){
     let roundNumber = mills > 0 ? Math.floor : Math.ceil;
     let days = roundNumber(mills / 86400000),
@@ -26,10 +33,7 @@ module.exports = async (bot, message, guild) => {
     time += (secs > 0) ? `${secs} Seconds` : "0 Seconds";
     return time;
   }
-  
-  if(data) {
-    const prefix = data.Prefix;
-   
+
     let uptime = convertMs(message.client.uptime);
     let ramUsage = (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2) + "MB";
       
@@ -62,51 +66,5 @@ module.exports = async (bot, message, guild) => {
 
     if (command) 
         command.run(bot, message, args);
-    
- 
-   
-
-  }
-  else if (!data) {
-
-    const prefix = "#";
-    
-    let uptime = convertMs(message.client.uptime);
-    let ramUsage = (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2) + "MB";
-      
-      const embedBot = new Discord.MessageEmbed ()
-      
-        .setTitle(`Ola sou o Illumination`)
-        .setDescription(`Se você me mencionou é porque esqueceu o prefixo ou é a minha primeira vez nesse Servidor`)
-        .addFields(
-          { name: "Prefixo", value:  "```" + prefix + "```", inline: true },
-          { name: "Guilds", value: "```" + bot.guilds.cache.size + "```", inline: true},
-          { name: "Users", value:  "```" + bot.users.cache.size + "```", inline: true },
-          { name: "RAM usage", value:  "```" + ramUsage + "```", inline: true },
-          { name: "API Latency", value:  "```" + bot.ws.ping + "```", inline: true },
-          { name: "Para saber os comandos ou obeter ajuda use:", value:  "```" + `${prefix}help` + "```", inline: false},
-          { name: "Built using", value:  "```" + `Node.js: V${process.versions.node}, Discord.js: V${Discord.version}, Mongoose: V${mongoose.version}` + "```", inline: false },
-          { name: "Uptime", value:  "```" + uptime + "```", inline: false },
-        )
-        .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
-        .setTimestamp()
-        .setColor('FF007F')
-        
-    if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) return message.channel.send(embedBot);
-    if (!message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd.length === 0) return;
-    let command = bot.commands.get(cmd);
-    if (!command) command = bot.commands.get(bot.aliases.get(cmd));
-
-    if (command) 
-        command.run(bot, message, args);
-    
-
   
-   
-   }
-
 };
